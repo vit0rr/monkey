1f0431997e47b3b6f84b0413802443a9356e4685
vitor <vi.souza.almeida@protonmail.com>
2024-09-15 13:10:32 -0300

    feat(repl): return tokens of the code

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/vit0rr/mumu/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
