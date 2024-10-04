65585ff9e2cbd1047e2e1dff7b2ba6ffd94738a7
vitor <vi.souza.almeida@protonmail.com>
2024-10-04 14:42:04 -0300

    feat(ecr): functions describe the world

--- object/object.go
+++ object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/vit0rr/mumu/ast"
+)
 
 type ObjectType string
 
@@ -10,6 +16,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTIONS_OBJ    = "FUNCTIONS"
 )
 
 type Object interface {
@@ -50,21 +57,27 @@ type Error struct {
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
-type Environment struct {
-	store map[string]Object
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s}
-}
+func (f *Function) Type() ObjectType { return FUNCTIONS_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
 
-func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.store[name]
-	return obj, ok
-}
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
 
-func (e *Environment) Set(name string, val Object) Object {
-	e.store[name] = val
-	return val
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
