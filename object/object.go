031438227eb064672002f4c59c48230f5d809e27
vitor <vi.souza.almeida@protonmail.com>
2024-10-11 20:19:35 -0300

    feat(hash): hash inspect function

--- object/object.go
+++ object/object.go
@@ -161,3 +161,21 @@ type Hash struct {
 }
 
 func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
+
+type Hashable interface {
+	HashKey() HashKey
+}
