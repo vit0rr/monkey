5f707be35126a72b04f28b2d9775fbdce8c0ee6f
vitor <vi.souza.almeida@protonmail.com>
2024-10-24 19:15:25 -0300

    feat(compiler): compare instructions

---
+++ compiler/compiler.go
@@ -0,0 +1,35 @@
+package compiler
+
+import (
+	"github.com/vit0rr/mumu/ast"
+	"github.com/vit0rr/mumu/code"
+	"github.com/vit0rr/mumu/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
