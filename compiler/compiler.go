4bf0aead600b2e25eab15eb3e58d3e9b912edee3
vitor <vi.souza.almeida@protonmail.com>
2024-11-01 10:56:03 -0300

    feat(compiler): compile infix expressions (#22)

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -51,6 +51,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		switch node.Operator {
 		case "+":
 			c.emit(code.OpAdd)
+		case "-":
+			c.emit(code.OpSub)
+		case "*":
+			c.emit(code.OpMul)
+		case "/":
+			c.emit(code.OpDiv)
 		default:
 			return fmt.Errorf("unknow operador %s", node.Operator)
 		}
